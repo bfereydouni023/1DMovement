0f1c9a9e76ac6a76862fde592cf30d2cd56e5924
bfereydouni023 <bfereydo@gmail.com>
2025-04-23 17:16:18 -0700

    Add files via upload

---
+++ src/Scenes/1DMovement.js
@@ -0,0 +1,21 @@
+//Behnood Fereydouni ID#1913257
+class Movement extends Phaser.Scene {
+    constructor() {
+        super("movementScene");
+
+    }
+
+    // Use preload to load art and sound assets before the scene starts running.
+    preload() {
+
+    }
+
+    create() {
+        let my = this.my;   // create an alias to this.my for readability
+
+    }
+
+    update() {
+        let my = this.my;
+    }
+}
